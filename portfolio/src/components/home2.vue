d382179f0b74cc292c610133f58178bafd2b9c53
Tommyscripts <dark.angel.alhue@gmail.com>
2023-01-26 09:46:58 +0000

    testing

---
+++ portfolio/src/components/home2.vue
@@ -0,0 +1,57 @@
+<template>
+    <v-card
+      :loading="loading"
+      class="mx-auto my-12"
+      max-width="374"
+    >
+      <template v-slot:loader="{ isActive }">
+        <v-progress-linear
+          :active="isActive"
+          color="deep-purple"
+          height="4"
+          indeterminate
+        ></v-progress-linear>
+      </template>
+  
+      <v-img
+        cover
+        height="250"
+        src="../img/me.jpg"
+      ></v-img>
+  
+      <v-card-item>
+        <v-card-title>Javier Cabrera Escoz</v-card-title>
+  
+      </v-card-item>
+  
+      <v-card-text>
+  
+        <div>Siempre me ha llamado la atención la tecnología y la informática, por cosas de la vida nunca pude dedicarme ni aprender a programar,
+            debido a esto unos meses antes de la pandemia decidi hacer un curso de programación web, el cual tuvo sus problemas debido a la pandemia, y a otras circunstancias, poco despues consegui una subención para hacer un bootcamp, el cual realize sin pensarme dos veces, gracias a eso aprendí a programar.
+            <br>
+            Algo más sobre mi, me encanta leer, escribir, los videojuegos, y me puedo enganchar a una serie como nadie.
+
+        </div>
+      </v-card-text>
+  
+      <v-divider class="mx-4 mb-1"></v-divider>
+  
+    </v-card>
+  </template>
+
+<script>
+export default {
+  data: () => ({
+    loading: false,
+    selection: 1,
+  }),
+
+  methods: {
+    reserve () {
+      this.loading = true
+
+      setTimeout(() => (this.loading = false), 2000)
+    },
+  },
+}
+</script>
---
+++ portfolio/src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <Menu />
+  <home2 />
+</template>
+
+<script setup>
+  import Menu from '@/components/Menu.vue'
+  import home2 from '@/components/home2.vue'
+</script>
