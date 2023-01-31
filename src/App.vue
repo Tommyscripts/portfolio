2f9260f9434c5b7e47249bf051e1d13a6e42561b
Tommyscripts <dark.angel.alhue@gmail.com>
2023-01-31 10:55:19 +0000

    New view

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,36 @@
-<template >
-  <v-app >
+<template>
+  <v-app>
     <v-main>
-      <v-row>
-        <v-col cols="12">
-          <Menu />
-        </v-col>
-      </v-row>
-      <v-row><v-col cols="12">
-        <RouterView />
-      </v-col>
-      </v-row>
+      <v-container>
+        <v-row no-gutters>
+          <v-col order="last">
+            <v-img cover height="250" src="../img/me.jpg"></v-img>
+          </v-col>
+          <v-col>
+            <v-row
+              ><v-col cols="12">
+                <RouterView />
+              </v-col>
+            </v-row>
+          </v-col>
+          <v-col order="first">
+            <Menu />
+          </v-col>
+        </v-row>
+      </v-container>
       <v-row>
         <v-col cols="12">
           <Footer />
         </v-col>
       </v-row>
     </v-main>
-</v-app>
+  </v-app>
 </template>
 
 <script setup>
-  import { RouterLink, RouterView } from "vue-router";
-  import Menu from '@/components/Menu.vue'
-  import Footer from '@/components/footer.vue'
-  
+import { RouterLink, RouterView } from "vue-router";
+import Menu from "@/components/Menu.vue";
+import Footer from "@/components/footer.vue";
 </script>
 <script>
 export default {
--- src/components/home.vue
+++ src/components/home.vue
@@ -1,54 +1,34 @@
 <template>
-  <v-card :loading="loading" class="mx-auto my-12" max-width="374" color="#009688">
-    <template v-slot:loader="{ isActive }">
-      <v-progress-linear
-        :active="isActive"
-        color="deep-purple"
-        height="4"
-        indeterminate
-      ></v-progress-linear>
-    </template>
-
-    <v-img cover height="250" src="../img/me.jpg"></v-img>
-
-    <v-card-item>
-      <v-card-title>Javier Cabrera Escoz</v-card-title>
-    </v-card-item>
-
-    <v-card-text>
-      <div>
-        Siempre me ha llamado la atención la tecnología y la informática, por
-        cosas de la vida nunca pude dedicarme ni aprender a programar, debido a
-        esto unos meses antes de la pandemia decidi hacer un curso de
-        programación web, el cual tuvo sus problemas debido a la pandemia, y a
-        otras circunstancias, poco despues consegui una subención para hacer un
-        bootcamp, el cual realize sin pensarme dos veces, gracias a eso aprendí
-        a programar.
-        <br />
-        Algo más sobre mi, me encanta leer, escribir, los videojuegos. Una de
-        mis sagas favoritas es Metal Gear, me encanta el cine y ver series en mi
-        tiempo libre.
-      </div>
-    </v-card-text>
-
-    <v-divider class="mx-4 mb-1"></v-divider>
-  </v-card>
-
+  <v-container>
+    <v-row no-gutters>
+      <v-col order="last">
+        <v-img cover height="250" src="../img/me.jpg"></v-img>
+      </v-col>
+      <v-col>
+        <v-card-item>
+          <v-card-title>Javier Cabrera Escoz</v-card-title>
+        </v-card-item>
+        <v-card-text>
+          <div>
+            Siempre me ha llamado la atención la tecnología y la informática,
+            por cosas de la vida nunca pude dedicarme ni aprender a programar,
+            debido a esto unos meses antes de la pandemia decidi hacer un curso
+            de programación web, el cual tuvo sus problemas debido a la
+            pandemia, y a otras circunstancias, poco despues consegui una
+            subención para hacer un bootcamp, el cual realize sin pensarme dos
+            veces, gracias a eso aprendí a programar.
+            <br />
+            Algo más sobre mi, me encanta leer, escribir, los videojuegos. Una
+            de mis sagas favoritas es Metal Gear, me encanta el cine y ver
+            series en mi tiempo libre.
+          </div>
+        </v-card-text>
+      </v-col>
+    </v-row>
+  </v-container>
+  <v-divider class="mx-4 mb-1"></v-divider>
 </template>
 
 <script>
-export default {
-  data: () => ({
-    loading: false,
-    selection: 1,
-  }),
-
-  methods: {
-    reserve() {
-      this.loading = true;
-
-      setTimeout(() => (this.loading = false), 2000);
-    },
-  },
-};
+export default {};
 </script>
