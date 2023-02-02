bb6ea5a009f7eb7edc7313f4ca44a1dbefa38d31
Tommyscripts <dark.angel.alhue@gmail.com>
2023-02-02 11:58:32 +0000

    fixing some issues and added a new view for my projects

--- src/components/Aboutme.vue
+++ src/components/Aboutme.vue
@@ -5,33 +5,28 @@
         En 2019 termine mis estudios de telecomunicaciones
       </v-card-text>
       </v-timeline-item>
-
     <v-timeline-item>
       <v-card-text>
         En 2020 comenzo mi gusto por la programación
       </v-card-text>
      </v-timeline-item>
-
     <v-timeline-item>
       <v-card-text>
         Tras la pandemia en 2021 consegui mi primer trabajo en
       sistemas
       </v-card-text>
     </v-timeline-item>
-
     <v-timeline-item>
       <v-card-text>
         En 2022 hice varios cursos, y termine el año acabando el bootcam con
       Reboot Academy
       </v-card-text>
     </v-timeline-item>
-
     <v-timeline-item>
       <v-card-text>
         En 2023 conseguí mi primer trabajo de programador
       </v-card-text>
       </v-timeline-item>
-
     <v-timeline-item>
       <v-card-text>
         Si quiere saber mas de mi haz click aquí para descargar mi curriculum
@@ -39,13 +34,12 @@
         rounded
         color="light-blue darken-1"
         elevation="2"
+        rel="pdf"
         href="@/files/JAVIER_CV.pdf"
         download="JAVIER_CV.pdf">
         click
         </v-btn>
-
       </v-card-text>
-     
     </v-timeline-item>
   </v-timeline>
 </template>
--- src/components/Myprojects.vue
+++ src/components/Myprojects.vue
@@ -1,45 +1,93 @@
 <template>
-    <v-container >
-    <v-row id="caja">
-      <v-col
-        v-for="elevation in elevations"
-        :key="elevation"
-        cols="12"
-        md="4"
-      >
-        <v-sheet
-          class="pa-12"
-          color="grey lighten-3"
-        >
-          <v-sheet
-            :elevation="elevation"
-            class="mx-auto"
-            height="100"
-            width="100"
-          ></v-sheet>
-        </v-sheet>
-      </v-col>
-    </v-row>
-    <v-row id="caja">
-      <v-col
-        v-for="elevation in elevations"
-        :key="elevation"
-        cols="12"
-        md="4"
-      >
-        <v-sheet
-          class="pa-12"
-          color="grey lighten-3"
-        >
-          <v-sheet
-            :elevation="elevation"
-            class="mx-auto"
-            height="100"
-            width="100"
-          ></v-sheet>
-        </v-sheet>
-      </v-col>
-    </v-row>
+      <v-container
+    class="fill-height"
+    fluid
+    style="min-height: 434px"
+    id="caja"
+  >
+    <v-fade-transition mode="out-in">
+      <v-row>
+        <v-col cols="6">
+          <v-card>
+            <v-img
+              src="https://picsum.photos/350/165?random"
+              height="125"
+              class="bg-grey-lighten-2"
+            ></v-img>
+            <v-card-title class="text-h6">
+              height
+            </v-card-title>
+          </v-card>
+        </v-col>
+
+        <v-col cols="6">
+          <v-card>
+            <v-img
+              src="https://picsum.photos/350/165?random"
+              height="125"
+              cover
+              class="bg-grey-lighten-2"
+            ></v-img>
+            <v-card-title class="text-h6">
+              height with cover
+            </v-card-title>
+          </v-card>
+        </v-col>
+
+        <v-col cols="6">
+          <v-card>
+            <v-img
+              src="https://picsum.photos/350/165?random"
+              max-height="125"
+              class="bg-grey-lighten-2"
+            ></v-img>
+            <v-card-title class="text-h6">
+              max-height
+            </v-card-title>
+          </v-card>
+        </v-col>
+
+        <v-col cols="6">
+          <v-card>
+            <v-img
+              src="https://picsum.photos/350/165?random"
+              max-height="125"
+              cover
+              class="bg-grey-lighten-2"
+            ></v-img>
+            <v-card-title class="text-h6">
+              max-height with cover
+            </v-card-title>
+          </v-card>
+        </v-col>
+        <v-col cols="6">
+          <v-card>
+            <v-img
+              src="https://picsum.photos/350/165?random"
+              max-height="125"
+              cover
+              class="bg-grey-lighten-2"
+            ></v-img>
+            <v-card-title class="text-h6">
+              max-height with cover
+            </v-card-title>
+          </v-card>
+        </v-col>
+        <v-col cols="6">
+          <v-card>
+            <v-img
+              src="https://picsum.photos/350/165?random"
+              max-height="125"
+              cover
+              class="bg-grey-lighten-2"
+            ></v-img>
+            <v-card-title class="text-h6">
+              max-height with cover
+            </v-card-title>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-fade-transition>
   </v-container>
 </template>
 
